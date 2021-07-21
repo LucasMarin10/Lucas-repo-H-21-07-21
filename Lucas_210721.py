9d852d7d06cba9511833715e1eefe920bd82a00a
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-07-21 11:04:43 -0300

    Add files via upload

---
+++ Lucas_210721.py
@@ -0,0 +1,22 @@
+print("question 2")
+usn=input("what is your name?")
+print(usn)
+print("question 3")
+print("Hello,"+" "+usn+" Thanks for playing my code")
+print("question 4")
+tftu=input(usn+" write some text")
+print(tftu)
+print("question 5")
+for a in range(10):
+    print(tftu.upper()+"!")
+print("question 6")
+lmbu=input(usn+" write a letter")
+print("question 7")
+wol=""
+for s in range(20):
+    wol=wol+" "+lmbu
+print(wol)
+print("question 8")
+nilu=input(usn+" write a name in lower case")
+print("question 9")
+print(nilu.title())
